156b8b1d510259408e428a0d9c219f4b2a979809
riparino <daniel_rico@outlook.com>
2025-04-08 19:56:52 -0700

    init commit

---
+++ SendSyslogPayload.ps1
@@ -0,0 +1,9 @@
+$logFile = ".\securden.txt"
+$server = ""
+$port = 514
+
+Get-Content $logFile | ForEach-Object {
+    echo $_ | & ncat --udp --send-only $server $port
+    Write-Host $_
+    Start-Sleep -Milliseconds 300
+}
